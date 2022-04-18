64fc33b99d5c31d3da077b026e22f2464a02c4b9
Murshedul <murshedbinamir@gmail.com>
2022-04-18 23:44:03 +0600

    404 edition

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -4,8 +4,8 @@ const NotFound = () => {
     return (
         <div className='container d-flex justify-content-center align-items-center' style={{ height: '500px' }}>
             <div>
-                <h2>404</h2>
-                <p>Sorry, the page is not found</p>
+                <h2 className='text-center' style={{fontSize: '58px'}}>404</h2>
+                <p style={{fontSize: '28px'}}>Sorry, the page is not found</p>
             </div>
         </div>
     );
