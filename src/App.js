cbdbf59b3288f70af6395f22eed1862206e71f26
Murshedul <murshedbinamir@gmail.com>
2022-04-18 23:40:42 +0600

    404 page added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Footer from './pages/shared/Footer/Footer';
 import Header from './pages/shared/Header/Header';
 import About from './pages/About/About';
 import Blogs from './pages/Blogs/Blogs';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
         <Route path='register' element={<Register></Register>}></Route>
         <Route path='about' element={<About></About>}></Route>
         <Route path='blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='container d-flex justify-content-center align-items-center' style={{ height: '500px' }}>
+            <div>
+                <h2>404</h2>
+                <p>Sorry, the page is not found</p>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
